a6fbfc1842c77a6f8b6099353f1c174ce7aa088e
Prabhav Doma <prabhav.doma@gmail.com>
2025-10-07 03:59:02 -0700

    fixed bugs and added default case

--- SignExtender.sv
+++ SignExtender.sv
@@ -1,20 +1,14 @@
 module immGeneration (input logic [31:0] instr, 
-			output logic imm);
-
-	logic [6:0] opcode;
+			output logic [31:0] imm);
 
 	always_comb begin
-		opcode = isntr[6:0];
-		
-		case(opcode)
+		case(instr[6:0])
 			7'b0110011: imm = 32'b0; 					//R-type (reg-reg)
-			7'b0000011: imm = {20{instr[31]}, instr[31:20]};  		//I-type (load)
-			7'b0010011: imm = {20{instr[31]}, instr[31:20]};		//I-type (immediate ALU ops)
-			7'b0100011: imm = {20{instr[31]}, instr[31:25], instr[11:7]};	//S-type (store)
-			7'1100011:  imm = {20{instr[31]}, instr[31:20]};		//B-type (branch)
-
-
-
-
-
-end
+			7'b0000011: imm = {{20{instr[31]}}, instr[31:20]};  		//I-type (load)
+			7'b0010011: imm = {{20{instr[31]}}, instr[31:20]};		//I-type (immediate ALU ops)
+			7'b0100011: imm = {{20{instr[31]}}, instr[31:25], instr[11:7]};	//S-type (store)
+			7'b1100011: imm = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};		//B-type (branch)
+			default:    imm = 32'b0;
+		endcase
+	end
+endmodule
